31209e17fd463fb5d10e66d6a0cc73537ff943a1
MohamedMamdouh-98 <miidom96@gmail.com>
2023-12-02 17:22:29 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// get inputs
+let title = document.getElementById("title");
+price = document.getElementById("price");
+ads = document.getElementById("ads");
+discount = document.getElementById("discount");
+taxes = document.getElementById("taxes");
+total = document.getElementById("total");
+count = document.getElementById("count");
+category = document.getElementById("category");
+create = document.getElementById("create");
+
+// get total
