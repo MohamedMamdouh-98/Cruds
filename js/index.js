3151ec9f66b71730b97f89b9f7689987c6198b4d
MohamedMamdouh-98 <miidom96@gmail.com>
2023-12-03 12:36:46 +0200

    [create] createProduct function

--- js/index.js
+++ js/index.js
@@ -7,6 +7,38 @@ taxes = document.getElementById("taxes");
 total = document.getElementById("total");
 count = document.getElementById("count");
 category = document.getElementById("category");
-create = document.getElementById("create");
-
+submit = document.getElementById("submit");
 // get total
+getTotal = () => {
+  if (price.value != "") {
+    let result = +price.value + +ads.value + +taxes.value - +discount.value;
+    total.innerHTML = result;
+    total.classList.remove("bg-danger");
+    total.classList.add("bg-success");
+  } else {
+    total.innerHTML = "";
+    total.classList.remove("bg-success");
+    total.classList.add("bg-danger");
+  }
+};
+
+//create product
+let dataProduct;
+localStorage.product != null
+  ? (dataProduct = JSON.parse(localStorage.product))
+  : (dataProduct = []);
+
+submit.addEventListener("click", () => {
+  let newProduct = {
+    title: title.value,
+    price: price.value,
+    taxes: taxes.value,
+    ads: ads.value,
+    discount: discount.value,
+    total: total.textContent,
+    count: count.value,
+    category: category.value,
+  };
+  dataProduct.push(newProduct);
+  localStorage.setItem("product", JSON.stringify(dataProduct));
+});
